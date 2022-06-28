a137510c07d0deb4425b015e51b7e332c1d7fc95
chemin <chemin0428@gmail.com>
2022-06-28 09:30:07 +0900

    배포 테스트

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,21 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Switch } from 'react-router-dom';
 import Coin from './routes/Coin';
 import Coins from './routes/Coins';
 
 function Router() {
 	return (
-		<BrowserRouter>
-			<Switch>
-				<Route path="/:coinId">
-					<Coin />
-				</Route>
-				<Route path="/">
-					<Coins />
-				</Route>
-			</Switch>
-		</BrowserRouter>
+		<HashRouter>
+			<BrowserRouter>
+				<Switch>
+					<Route path="/:coinId">
+						<Coin />
+					</Route>
+					<Route path="/">
+						<Coins />
+					</Route>
+				</Switch>
+			</BrowserRouter>
+		</HashRouter>
 	);
 }
 
